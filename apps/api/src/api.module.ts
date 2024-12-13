d3bd2cec7c8933cad18addc34a67f4ef517eaede
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-12-14 02:56:38 +0300

    feat: rabbitmq

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from '@app/shared';
 import { ApiService } from './api.service';
 import { ApiController } from './api.controller';
+import { SharedModule } from '@app/shared/modules/shared.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [SharedModule, ConfigModule.forRoot()],
   controllers: [ApiController],
   providers: [ApiService, PrismaService],
 })
--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,6 +1,7 @@
 export * from './docs/docs.config';
 
 export * from './services/prisma.service';
+export * from './services/shared.service';
 
 export * from './utils/logger/logger-winston.config';
 export * from './utils/logger/logger.interceptor';
